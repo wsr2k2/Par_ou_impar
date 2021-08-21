eeaa93fa8cbbbabf298f3328f2358aaaadec02b2
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-08-21 00:55:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+n1 = parseInt(prompt('Digite um numero: '));
+let res = '';
+if (n1 % 2 === 0){
+ res = 'PAR'
+ console.log(`Você digitou o numero ${n1} e esse numero é: ${res}!`);
+} else if (n1 % 2 === 1){
+  res = 'ÍMPAR'
+  console.log(`Você digitou o numero ${n1} e esse numero é: ${res}!`);
+} else {
+  console.log('Você digitou uma opção invalida! Tente novamente!')
+};
